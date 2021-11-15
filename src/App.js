f4cfae58e4078014c1ed269e0208d79e53844f45
rajachoudhary <rajachoudhary341@gmail.com>
2021-11-15 18:38:32 +0530

    react-intro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import Button from './Components/Button';
+import icon from './svg/download.jpg';
 
 
 function App() {
@@ -9,9 +10,9 @@ function App() {
       <Button title="A"/>
       <Button title="B"/>
       <Button title="D"/>
-      
-     
-      
+      <img src="logo192.png" alt="logo"/>
+      <img src={icon} alt="react"></img>
+      <div>raja</div>
     </div>
   );
 }
